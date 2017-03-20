2dded540c9a7dfd55678e9d13fea45d722a7e496
webadam1 <webadam1@gmail.com>
2017-03-20 07:16:24 -0700

    Implemented timer calling followSpeed

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,12 +1,15 @@
 package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private Timer timer = new Timer();
 
 	@Override
 	public void followSpeed() {
@@ -43,7 +46,13 @@ public class TrainControllerImpl implements TrainController {
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
+		this.step = joystickPosition;
+		timer.scheduleAtFixedRate(new TimerTask(){
+			@Override
+            public void run() {
+				followSpeed();
+            }
+		}, 1000, 1000);
 	}
 
 }
